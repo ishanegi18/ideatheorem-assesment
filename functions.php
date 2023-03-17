e4c564a6f1f946732db64a8fef758948459b43d6
Isha Negi <ishanegi18@gmail>
2023-03-16 20:00:50 -0400

    typo and mixins added

--- functions.php
+++ functions.php
@@ -11,6 +11,12 @@ add_action( 'init', 'disable_comments' );
 
 // Enqueue stylesheets and scripts
 function ideatheorem_scripts() {
+
+
+// Enqueue font style
+    wp_enqueue_style('google-font-montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap', array(), null);
+
+
     // Enqueue the main stylesheet
     wp_enqueue_style( 'main-styles', get_template_directory_uri(). '/css/style.css' );
     
@@ -18,3 +24,39 @@ function ideatheorem_scripts() {
     wp_enqueue_script( 'main-script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'ideatheorem_scripts' );
+
+
+
+
+
+  
+
+  function custom_theme_customizer( $wp_customize ) {
+    $wp_customize->add_setting( 'logo_image' );
+  
+    $wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'logo_image', array(
+      'label' => __( 'Logo Image', 'custom-theme' ),
+      'section' => 'title_tagline',
+      'settings' => 'logo_image',
+    ) ) );
+  }
+  
+  add_action( 'customize_register', 'custom_theme_customizer' );
+  
+  
+
+
+  add_theme_support( 'menus' );
+
+  function register_my_menus() {
+    register_nav_menus(
+      array(
+        'primary' => __( 'Primary Menu' ),
+        'footer' => __( 'Footer Menu' ),
+      )
+    );
+  }
+  add_action( 'init', 'register_my_menus' );
+  
+
+
--- template-parts/header.php
+++ template-parts/header.php
@@ -1,6 +1,24 @@
 <header>
-		<h1><?php bloginfo('name'); ?></h1>
-		<nav>
-			<?php wp_nav_menu(); ?>
-		</nav>
+<?php if ( get_theme_mod( 'logo_image' ) ) : ?>
+  <div class="site-logo">
+    <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+      <img src="<?php echo esc_url( get_theme_mod( 'logo_image' ) ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
+    </a>
+  </div>
+<?php endif; ?>
+
+
+
+
+
+<?php
+if ( has_nav_menu( 'primary' ) ) {
+  wp_nav_menu( array(
+    'theme_location' => 'primary',
+    'menu_class' => 'primary-menu',
+    'container' => true,
+  ) );
+}
+?>
+
 	</header>
